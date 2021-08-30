0d12a5d77bff175695893a1e32797c0803e132b7
Pranav Rayudu <tarakapranav@gmail.com>
2021-08-29 22:23:54 -0500

    switched icons and updated manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,13 +112,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Pranav Rayudu's Blog`,
+        short_name: `Pranav Rayudu`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/pranavr-logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
